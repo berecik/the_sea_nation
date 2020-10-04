14c422f4345832f826c5e59cb44af35541d573c5
beret <beret@hipisi.org.pl>
2020-10-04 17:21:11 +0100

    move SECRET_KEY to enviroment

--- the_sea_nations/settings/default_settings.py
+++ the_sea_nations/settings/default_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n#8dqxh6t8xcn1h&genao91b*+k92g9wjdvs*93cn6xqps*dr+'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'xxx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
